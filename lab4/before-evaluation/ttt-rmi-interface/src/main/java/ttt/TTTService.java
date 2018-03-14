0377da49fc5c04fa6ddab4b3041c67ce634eb51c
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2018-03-14 15:43:39 +0000

    evaluation and lab before evaluation

--- lab4/before-evaluation/ttt-rmi-interface/src/main/java/ttt/TTTService.java
+++ lab4/before-evaluation/ttt-rmi-interface/src/main/java/ttt/TTTService.java
@@ -7,6 +7,7 @@ import java.rmi.RemoteException;
 public interface TTTService extends Remote {
     String currentBoard() throws RemoteException;
     boolean play(int row, int column, int player) throws RemoteException;
+    boolean play(int row, int column, int player) throws RemoteException;
     int checkWinner() throws RemoteException;
     
 
